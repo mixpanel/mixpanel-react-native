c0b980491167f3e8980520045b246208425ea5ef
Amit Jadhav <ajadhav@tapsensetech.com>
2019-08-07 15:23:01 +0530

    Mixpanel demo app changes

--- MixpanelDemo/android/app/src/main/java/com/mixpaneldemo/MainApplication.java
+++ MixpanelDemo/android/app/src/main/java/com/mixpaneldemo/MainApplication.java
@@ -3,6 +3,7 @@ package com.MixpanelDemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.mixpanel.reactnative.MixpanelReactNativePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MixpanelReactNativePackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );

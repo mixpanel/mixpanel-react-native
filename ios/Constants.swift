7c07dea8a29968d02972e4a07828e59f0858bef6
Gayatri Lokhande <glokhande@tapsensetech.com>
2019-08-08 18:09:18 +0530

    constants modified

--- ios/Constants.swift
+++ ios/Constants.swift
@@ -1,6 +1,6 @@
 struct Constants {
     static let ERROR = "ERROR: "
-    static let INSTANCE_NOT_FOUND_ERROR  = "You have to call `getInstance(token)` before calling methods on Mixpanel Instance"
+    static let INSTANCE_NOT_FOUND  = "You have to call `getInstance(token)` before calling methods on Mixpanel Instance"
     static let LIBRARY_INVOKED  = "iOS library invoked successfully"
 }
 

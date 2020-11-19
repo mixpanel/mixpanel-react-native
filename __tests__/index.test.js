641d123f7d2c242f03428fb0fe461e67c42f1e43
Zihe Jia <zihe.jia@mixpanel.com>
2020-11-19 15:40:18 -0800

    wip

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+ 
+
+const NativeModules = require('react-native').NativeModules;
+const EventEmitter = require('EventEmitter');
+const Mixpanel = require('../index');
+const People = require('../index');
+
+// jest.mock('NativeModules', () => {
+//     return {
+//       MixpanelReactNative: {
+//         optOutTracking: jest.fn()
+//       }
+//     };
+// });
+
+
+test('renders correctly', () => {
+    Mixpanel.optOutTracking();
+});

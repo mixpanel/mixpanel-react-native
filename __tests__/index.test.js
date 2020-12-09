bb16824840ad2a7c75a393f8c0204544d92b596a
Zihe Jia <zihe.jia@mixpanel.com>
2020-12-08 23:52:04 -0800

    bump to 1.0.0

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,7 @@ import { NativeModules } from 'react-native';
 
 test(`it calls MixpanelReactNative initialize`, async () => {
   const mixpanel = await Mixpanel.init("token");
-  expect(NativeModules.MixpanelReactNative.initialize).toBeCalledWith("token", false, {"$lib_version": "1.0.0-beta.9", "mp_lib": "react-native"});
+  expect(NativeModules.MixpanelReactNative.initialize).toBeCalledWith("token", false, {"$lib_version": "1.0.0", "mp_lib": "react-native"});
 });
 
 test(`it calls MixpanelReactNative hasOptedOutTracking`, async () => {

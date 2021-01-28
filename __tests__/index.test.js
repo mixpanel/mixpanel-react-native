7ef5cee106fd0e1dd8ca6a6c16725204ed404752
Zihe Jia <zihe.jia@mixpanel.com>
2021-01-27 17:00:21 -0800

    Version 1.0.2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,7 @@ import { NativeModules } from 'react-native';
 
 test(`it calls MixpanelReactNative initialize`, async () => {
   const mixpanel = await Mixpanel.init("token");
-  expect(NativeModules.MixpanelReactNative.initialize).toBeCalledWith("token", false, {"$lib_version": "1.0.1", "mp_lib": "react-native"});
+  expect(NativeModules.MixpanelReactNative.initialize).toBeCalledWith("token", false, {"$lib_version": "1.0.2", "mp_lib": "react-native"});
 });
 
 test(`it calls MixpanelReactNative setServerURL`, async () => {

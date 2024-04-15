f39685c085a38d27f37cc01d0a68324c13dc94db
Zihe Jia <zihe.jia@mixpanel.com>
2024-04-15 12:13:35 -0700

    should use the correct object to initialize Mixpanel

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ export class Mixpanel {
     superProperties = {},
     serverURL = "https://api.mixpanel.com"
   ) {
-    await MixpanelReactNative.initialize(
+    await this.mixpanelImpl.initialize(
       this.token,
       this.trackAutomaticEvents,
       optOutTrackingDefault,
@@ -112,7 +112,7 @@ export class Mixpanel {
     trackAutomaticEvents,
     optOutTrackingDefault = DEFAULT_OPT_OUT
   ) {
-    await MixpanelReactNative.initialize(
+    await this.mixpanelImpl.initialize(
       token,
       trackAutomaticEvents,
       optOutTrackingDefault,

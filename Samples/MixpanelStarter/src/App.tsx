b28632e32a749e096a774d5d89333195d3306610
Kwame Efah <37164746+efahk@users.noreply.github.com>
2026-06-17 16:27:18 +0000

    Merge master into feature/add-feature-flags-support

--- Samples/MixpanelStarter/src/App.tsx
+++ Samples/MixpanelStarter/src/App.tsx
@@ -19,7 +19,7 @@ const token = MIXPANEL_TOKEN || DEMO_TOKEN;
 function App(): React.JSX.Element {
   return (
     <ErrorBoundary>
-      <MixpanelProvider token={token} trackAutomaticEvents={true} useNative={true}>
+      <MixpanelProvider token={token} trackAutomaticEvents={true} useNative={true} serverURL="https://api-eu.mixpanel.com">
         <NavigationContainer>
           <Tab.Navigator
             screenOptions={{
